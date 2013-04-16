c4b0b3d3f5b0d710884d968a99a2262a2a566b0a
Tom Switzer <thomas.switzer@gmail.com>
2013-04-16 15:50:58 -0600

    Add overview page for javadocs.

---
+++ src/main/java/com/precog/client/rest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * A simple REST client.
+ *
+ * This package provides a simple REST client that the Precog client library
+ * uses to access the Precog web services. It is not intended to be used by
+ * users of the Precog client library.
+ */
+package com.precog.client.rest;
